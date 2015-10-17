457780c23cb4f655bfc37959f5f5f2e4cd511e46
Pronyuk Mihail <mishaprk@gmail.com>
2015-10-17 18:52:01 +0300

    use Faker

--- index.php
+++ index.php
@@ -9,7 +9,26 @@ require 'vendor/autoload.php';
 
 use Carbon\Carbon;
 
+$faker = Faker\Factory::create('uk_UA');
+# $fake = Faker;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Home Work 2</title>
+</head>
+<body>
+<?php
 echo "<h3>Home Work 2</h3><br />";
 echo("Now in Kiev: ". Carbon::now()." || ");
 echo("Now in New York: ". Carbon::now('America/New_York')." || ");
 echo("Now in London: ". Carbon::now('Europe/London')."<hr />");
+echo '<b>Faker:</b><br />';
+echo "name: ".$faker->name."<br />";
+echo "address: ".$faker->address."<br /><br />";
+echo "text: ".$faker->text;
+# echo "Phone: ".$fake->phoneNumber;
+?>
+</body>
+</html>
