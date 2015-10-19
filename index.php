08a17af9e60d94fef2d9bc494b75fcce6e01f188
Mihail Pronyuk <mishaprk@gmail.com>
2015-10-19 14:14:35 +0300

    add if validate and PSR

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * Created by PhpStorm.
  * User: user
  * Date: 10/17/15
- * Time: 12:46 PM
+ * Time: 12:46 PM.
  */
 require 'vendor/autoload.php';
 
@@ -11,7 +11,6 @@ use Carbon\Carbon;
 use Respect\Validation\Validator as v;
 
 $faker = Faker\Factory::create('uk_UA');
-# $fake = Faker;
 ?>
 <!DOCTYPE html>
 <html>
@@ -21,12 +20,12 @@ $faker = Faker\Factory::create('uk_UA');
 </head>
 <body>
 <?php
-echo "<h3>Home Work 2</h3><br />";
-echo("Now in Kiev: ". Carbon::now()." || ");
-echo("Now in New York: ". Carbon::now('America/New_York')." || ");
-echo("Now in London: ". Carbon::now('Europe/London')."<hr />");
-echo "<b>Generating fake data:</b><br />";
-echo "Enter number of data you want (between 1 and 100)";
+echo '<h3>Home Work 2</h3>';
+echo('Now in <b>Kiev</b>: '.Carbon::now().' || ');
+echo('in <b>New York</b>: '.Carbon::now('America/New_York').' || ');
+echo('in <b>London</b>: '.Carbon::now('Europe/London').'<hr />');
+echo '<b>Generating fake data:</b><br />';
+echo 'Enter number of data you want (between 1 and 150)';
 ?>
 <form method="post" action="">
         <label for="number">Entet number</label>
@@ -34,20 +33,24 @@ echo "Enter number of data you want (between 1 and 100)";
         <input type="submit" value="Generate"/>
 </form><hr />
 <?php
-if(isset($_POST['number'])){
-    $num=1;
-        if(v::numeric()->validate($_POST['number'])) {
-        $n = $_POST['number'];
-        for ($i = 0; $i < $n; $i++) {
-            echo "<b>$num</b>name: " . $faker->name . "<br />";
-            echo "address: " . $faker->address . "<br /><br />";
-            echo "comments: " . $faker->text . "<br />";
-            $num++;
+if (isset($_POST['number'])) {
+    $num = 1;
+    if (v::string()->notEmpty()->validate($_POST['number'])) {
+        if (v::numeric()->positive()->between(0, 151)->validate($_POST['number'])) {
+            $n = $_POST['number'];
+            for ($i = 0; $i < $n; ++$i) {
+                echo "<b>$num</b>name: ".$faker->name.'<br />';
+                echo 'address: '.$faker->address.'<br /><br />';
+                echo 'comments: '.$faker->text.'<br />';
+                ++$num;
+            }
+        } else {
+            echo'you entered not correct data';
         }
+    } else {
+        echo 'you entered empty data';
     }
-    else echo"you entered not numeric data";
 }
-# echo "Phone: ".$fake->phoneNumber;
 ?>
 </body>
 </html>
