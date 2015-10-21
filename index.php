4b07d3f2dfda643a10435c6600bd81db92038dfc
Mihail Pronyuk <mishaprk@gmail.com>
2015-10-21 10:09:44 +0300

    logger testing

--- index.php
+++ index.php
@@ -11,7 +11,13 @@ use Carbon\Carbon;
 use Respect\Validation\Validator as v;
 
 $faker = Faker\Factory::create('uk_UA');
+$logger = new Katzgrau\KLogger\Logger(__DIR__ . '/logs');
+$logger->info('Some teeeext INFO');
+$logger->error('Some ERROr index');
+$logger->debug('Databases of users');
+
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -21,17 +27,18 @@ $faker = Faker\Factory::create('uk_UA');
 <body>
 <?php
 echo '<h3>Home Work 2</h3>';
-echo('Now in <b>Kiev</b>: '.Carbon::now().' || ');
-echo('in <b>New York</b>: '.Carbon::now('America/New_York').' || ');
-echo('in <b>London</b>: '.Carbon::now('Europe/London').'<hr />');
+echo('Now in <b>Kiev</b>: ' . Carbon::now() . ' || ');
+echo('in <b>New York</b>: ' . Carbon::now('America/New_York') . ' || ');
+echo('in <b>London</b>: ' . Carbon::now('Europe/London') . '<hr />');
 echo '<b>Generating fake data:</b><br />';
 echo 'Enter number of data you want (between 1 and 150)';
 ?>
 <form method="post" action="">
-        <label for="number">Entet number</label>
-        <input type="text" value="" name="number"/>
-        <input type="submit" value="Generate"/>
-</form><hr />
+    <label for="number">Entet number</label>
+    <input type="text" value="" name="number"/>
+    <input type="submit" value="Generate"/>
+</form>
+<hr/>
 <?php
 if (isset($_POST['number'])) {
     $num = 1;
@@ -39,13 +46,13 @@ if (isset($_POST['number'])) {
         if (v::numeric()->positive()->between(0, 151)->validate($_POST['number'])) {
             $n = $_POST['number'];
             for ($i = 0; $i < $n; ++$i) {
-                echo "<b>$num</b>name: ".$faker->name.'<br />';
-                echo 'address: '.$faker->address.'<br /><br />';
-                echo 'comments: '.$faker->text.'<br />';
+                echo "<b>$num</b>name: " . $faker->name . '<br />';
+                echo 'address: ' . $faker->address . '<br /><br />';
+                echo 'comments: ' . $faker->text . '<br />';
                 ++$num;
             }
         } else {
-            echo'you entered not correct data';
+            echo 'you entered not correct data';
         }
     } else {
         echo 'you entered empty data';
